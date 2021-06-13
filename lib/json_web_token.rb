9800f35118deb7a48f6d76ad324f7762ad51fcc0
sampatbadhe <sampat.badhe@kiprosh.com>
2021-06-13 11:02:22 +0530

    set jwt token default expiry to 1.day

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -14,17 +14,13 @@ class JsonWebToken
 
   # Validates the payload hash for expiration and meta claims
   def self.valid_payload(payload)
-    if expired(payload) || payload['iss'] != meta[:iss] || payload['aud'] != meta[:aud]
-      return false
-    else
-      return true
-    end
+    !(expired(payload) || payload['iss'] != meta[:iss] || payload['aud'] != meta[:aud])
   end
 
   # Default options to be encoded in the token
   def self.meta
     {
-      exp: 7.days.from_now.to_i,
+      exp: 1.day.from_now.to_i,
       iss: 'issuer_name',
       aud: 'client',
     }
